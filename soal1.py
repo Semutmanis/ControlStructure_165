8f2083c2db61bc3f3a76165d227145d9c306a432
Semutmanis <aldidermawan1@gmail.com>
2026-09-23 15:48:08 +0700

    soal kedua

---
+++ soal1.py
@@ -0,0 +1,13 @@
+Total=float(input("masukan nilai kamu dan temukan hasil kinerja dari kerja kamu : "))
+if Total >= 90:
+    print("Gacor king ")
+elif Total >= 80:
+    print("Terbaik lah kau man")
+elif Total >= 70:
+    print("Cukuplaah mang")
+elif Total >= 60:
+    print("Awas aja kalo kurang lagi")
+else:
+    print("Mintaa di hajar kau man")
+
+  
---
+++ soal2.py
@@ -0,0 +1,12 @@
+angka1=int(input("masukan angka pertama: "))
+angka2=int(input("masukan angka kedua: "))
+angka3=int(input("masukan angka ketiga: "))
+
+if angka1>angka2 and angka1>angka3:
+  print("angka pertama adalah yang terbesar")
+elif angka2>angka1 and angka2>angka3:
+  print("angka pertama adalah yang terbesar")
+elif angka3>angka1 and angka3>angka2:
+  print("angka pertama adalah yang terbesar")
+else:
+  print("tidak ada angka yang sam!!!")
